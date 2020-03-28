4447c09b1a11e9e00e7224d2d0f818b5643eba03
guivsouza <gvsouza@gmail.com>
2020-03-28 09:57:57 -0300

    funções avançadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,22 +1,56 @@
 const express = require('express')
+const { celebrate, Segments, Joi} = require('celebrate')
 
 const OngController = require('./controllers/OngControllers')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
 const SessionController = require('./controllers/SessionController')
 
+const {createOngSchema} = require('../src/validation/schemas/ongSchema')
+
+
 const routes = express.Router()
 
-routes.post('/sessions', SessionController.create)
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+        })
+    }), SessionController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
 
-routes.get('/profile', ProfileController.index)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: createOngSchema
+    }), OngController.create)
 
-routes.get('/incidents', IncidentController.index)
-routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+        }).unknown()
+    }), ProfileController.index)
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+        })
+    }),IncidentController.index)
+
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required()    
+        }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+        }).unknown()
+    }), IncidentController.create)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+      })
+    }),IncidentController.delete)
 
 
 
---
+++ backend/src/validation/schemas/ongSchema.js
@@ -0,0 +1,13 @@
+const {Joi} = require('celebrate')
+
+const createOngSchema = Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+})
+
+module.exports = { 
+  createOngSchema
+}
